29bd797f6c293a8f7605b68d1fa250703f02538a
Raquel Montero <raquel.montero@ucb.edu.bo>
2022-10-24 15:33:40 -0400

    new contribution

--- src/app/components/tracking/modal-new-donation/modal-new-donation.component.ts
+++ src/app/components/tracking/modal-new-donation/modal-new-donation.component.ts
@@ -2,6 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {ContributionConfigService} from '../../../services/contribution-config.service';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {ComboElement, ComboResponse} from '../../../models/Utils.model';
+import {UtilService} from '../../../services/util.service';
 
 export interface DialogData {
   id: string;
@@ -17,6 +19,7 @@ export interface DialogData {
 })
 export class ModalNewDonationComponent implements OnInit {
   contributionConfig: any;
+  paymentMethods: ComboElement[];
   donation = this.fb.group({
     contribution_record_id: null,
     tracking_assignment_id: [null, Validators.required],
@@ -37,18 +40,77 @@ export class ModalNewDonationComponent implements OnInit {
   });
   constructor(private fb: FormBuilder,
               @Inject(MAT_DIALOG_DATA) public data: DialogData,
+              private utilService: UtilService,
               private contributionconfigservice: ContributionConfigService) { }
 
   ngOnInit(): void {
     this.getContributionConfigById();
+    this.initData();
   }
 
+  initData(): void{
+    this.getPaymentMethods();
+  }
+
+  getPaymentMethods(): void {
+    this.utilService.getPaymentMethods()
+      .subscribe((data) => {
+        this.paymentMethods = data.data;
+      });
+  }
   getContributionConfigById(): void{
     this.contributionconfigservice.getContributionConfigById(
       this.data.contribution_config_id
     ).subscribe((data) => {
       this.contributionConfig = data;
       console.log('getContributionConfigById', data );
+    }, (error) => {
+      console.log('getContributionConfigById', error );
+      this.contributionConfig = {
+        contribution_config_id: 1,
+        contribution_key: 'APORTE_CONSTANTE',
+        contribution_id: '1',
+        contribution: {
+          const_contribution_id: '1',
+          start_month: 'DICIEMBRE',
+          paymentDate: 5,
+          remainderType: 'CORREO_ELECTRONICO',
+          contributionStartDate: '12/12/2022',
+          contributionEndDate: '12/12/2023',
+          contribution_amount: 35,
+          paymentMethod: 'CODIGO_QR',
+          send_news: true,
+          sendNewsType: 'MENSAJE_WHATSSAP'
+        }
+      };
+      this.donation.patchValue({
+        paymentMethod: this.contributionConfig.contribution.paymentMethod
+      });
     });
+  }
+
+  get donationAmount(): any {
+    return this.donation.get('contribution_ammount');
+  }
+
+  getDonationAmountError(): any {
+    if (this.donation.get('contribution_ammount').hasError('required')) {
+      return 'Debe ingresar el monto de aporte';
+    }
+  }
+  getPaymentDateError(): any {
+    if (this.donation.get('payment_date').hasError('required')) {
+      return 'Debe ingresar la fecha de aporte';
+    }
+  }
+  getExpectedPaymentDateError(): any {
+    if (this.donation.get('expected_payment_date').hasError('required')) {
+      return 'Debe ingresar la fecha esperada de aporte';
+    }
+  }
+  get childscount(): string{
+    let amount = this.donation.get('contribution_ammount').value;
+    amount = Math.trunc(Number(amount) / 35);
+    return amount + ' Niños';
   }
 }
