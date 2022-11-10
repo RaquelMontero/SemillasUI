644db14c870be85242bb10b0abea95251dcda43f
Raquel Montero <raquel.montero@ucb.edu.bo>
2022-11-10 16:26:59 -0400

    sidebar changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'sml-root',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
   title = 'semillasui';
   events: string[] = [];
-  opened: boolean;
+  opened = true;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
 }
